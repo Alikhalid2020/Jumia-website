58ef427993a5009f514a52837826514b8462ca90
praise-laurine <praiselaurine10@gmail.com>
2020-12-16 22:30:29 +0300

    made the sign up and login page

--- config.py
+++ config.py
@@ -1,24 +1,30 @@
 import os
+
 class Config:
-    """
-    general configuration parent class
-    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lola:1234@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 class ProdConfig(Config):
-    """
-    production configuration child clas
-    Args:
-       config: The parent configuration class with general configuration settings
-    """
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lola:1234@localhost/jumia_test'
+
 class DevConfig(Config):
-    """
-    development configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings.
-    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lola:1234@localhost/jumia'
     DEBUG = True
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-# 'test':TestConfig
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test':TestConfig
+
 }
